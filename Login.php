6556e3b0b07ea9b249a3bf9d0f41e20db9ec4efe
SeirinZ <kesatrianagaperak@gmail.com>
2024-01-16 10:08:57 +0700

    update 1 coba2

--- Login.php
+++ Login.php
@@ -1,3 +1,5 @@
+<!-- backend dimana -->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="" />
   <meta name="author" content="" />
-  <title>Login Page</title>
+  <title>Halaman Login</title>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <style>
     /* Custom styles */
